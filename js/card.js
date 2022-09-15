1f24a1ae4000bb307b6aab755d8dc6a059af8e33
khaitbek <khaitbek2005@gmail.com>
2022-09-15 18:01:57 +0500

    finish cards section

--- js/card.js
+++ js/card.js
@@ -1,11 +1,25 @@
 const NUMBER_OF_CARDS = 12
+const listViewToggler = document.querySelector("[data-view='list']")
+const gridViewToggler = document.querySelector("[data-view='grid']")
+const cardWrapper = document.querySelector("[data-card-wrapper]")
 
-for (let i = 0; i < NUMBER_OF_CARDS; i++) {
-    addCard()
-}
 
-function addCard() {
-    const cardWrapper = document.getElementById("cardWrapper")
-    const cardTemplate = document.querySelector("[data-card]").content.cloneNode(true).children[0]
-    cardWrapper.append(cardTemplate)
+function gridView() {
+    clearCards()
+    for (let i = 0; i < NUMBER_OF_CARDS; i++) {
+        let cardTemplate = document.querySelector("[data-card]").content.cloneNode(true).children[0]
+        addCard(cardTemplate, cardWrapper)
+    }
 }
+function addCard(template) { cardWrapper.append(template) }
+function listView() {
+    clearCards()
+    for (let i = 0; i < NUMBER_OF_CARDS; i++) {
+        let cardTemplate = document.querySelector("[data-list-card]").content.cloneNode(true).children[0]
+        addCard(cardTemplate)
+    }
+}
+function clearCards() { cardWrapper.innerHTML = "" }
+listViewToggler.addEventListener("click", listView)
+gridViewToggler.addEventListener("click", gridView)
+listView()
